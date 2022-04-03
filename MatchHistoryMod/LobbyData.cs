3df271ae3027638387b787f383d39f41c6dbd6c1
Jesper Ericsson <jjeessppeer@gmail.com>
2022-04-03 18:13:35 +0200

    record uploading now works

--- MatchHistoryMod/LobbyData.cs
+++ MatchHistoryMod/LobbyData.cs
@@ -26,70 +26,53 @@ namespace MatchHistoryMod
         public int TeamSize;
         public int TeamCount;
 
-        //public int CurrentPlayers;
-        //public int CurrentPilots;
-
         public ShipData[] Ships;
-        //public PlayerData[] Spectators = new PlayerData[4];
-        //public PlayerData[] AllPlayers = new PlayerData[20]; // 16 players + 4 spectators.
 
         public int[] Scores = new int[2];
+        public int Winner;
+        public int MatchTime;
 
         public int Status; // Lobby, Loading, Running, Ended
-        //public int? EndReason;
-        //public long StartDate;
-        public long? EndDate = null;
 
 
         public LobbyData(MatchLobbyView mlv, Mission mission)
         {
-            FileLog.Log($"Loading lobby data");
             MatchId = mlv.MatchId;
             MapName = mlv.Map.NameText.En;
+
             MapId = mlv.Map.Id;
             TeamSize = mission.shipsPerTeam;
             TeamCount = mission.numberOfTeams;
             Ships = new ShipData[TeamSize * TeamCount];
 
-            //CurrentPlayers = matchEntity.PlayerCount;
-            //CurrentPilots = matchEntity.CaptainCount;
-
             Status = 0;
             if (mlv.Loading) Status = 1;
             if (mlv.Running) Status = 2;
             if (mission.winningTeam != -1) Status = 3;
-
-
+            Winner = mission.winningTeam;
+            for (int i = 0; i < TeamCount; ++i)
+            {
+                Scores[i] = mission.TeamScore(i);
+            }
 
             //EndReason = (int?)matchEntity.EndReason;
             //StartDate = matchEntity.StartDate.Ticks;
             //if (matchEntity.EndDate.HasValue) EndDate = matchEntity.EndDate.Value.Ticks;
 
-            // Load all the crews
-
-            FileLog.Log($"Crews: {mlv.CrewShips.Count}");
-            //Crews = new ShipData[mlv.CrewShips.Count];
+            // Load all the ships and crews
             for (int i = 0; i < mlv.CrewShips.Count; ++i)
             {
-                FileLog.Log($"Trying to load ship {i}");
-
                 //if (mlv.CrewShips[i] == null || mlv.Crews[i] == null) ;
                 if (mlv.CrewShips[i] == null)
                 {
-                    FileLog.Log($"Null ship {i}");
                     continue;
                 }
                 if (mlv.FlatCrews[i] == null)
                 {
-                    FileLog.Log($"Null crew {i}");
                     continue;
                 }
-
-                FileLog.Log($"Loading ship");
                 Ships[i] = new ShipData(mlv.CrewShips[i], mlv.FlatCrews[i]);
-                FileLog.Log($"Ship loaded");
             }
-            FileLog.Log($"All ships loaded");
         }
     }
 
@@ -98,15 +81,15 @@ namespace MatchHistoryMod
         public int ShipModel;
         public int[] ShipLoadout = new int[6];
         public string ShipName;
+        public int Team;
 
         public PlayerData[] Players = new PlayerData[4];
 
         public ShipData(Muse.Goi2.Entity.Vo.CrewShipVO ship, CrewEntity crew)
         {
-            FileLog.Log($"Loading ship data {ship.ToString()}");
-
             ShipModel = ship.ModelId;
             ShipName = ship.Name;
+            Team = ship.Side;
             
             // Load ship loadout
             //crewship.Equipments
@@ -124,17 +107,14 @@ namespace MatchHistoryMod
             {
                 if (crew.Slots[i] == null)
                 {
-                    FileLog.Log($"Null crew slot {i}");
                     continue;
                 }
                 if (crew.Slots[i].PlayerEntity == null)
                 {
-                    FileLog.Log($"Null player {i}");
                     continue;
                 }
                 Players[i] = new PlayerData(crew.Slots[i]);
             }
-            FileLog.Log($"Ship complete");
         }
     }
 
@@ -149,9 +129,6 @@ namespace MatchHistoryMod
         public int Level;
 
         public List<int> Skills;
-        //public int[] PilotEquipment = new int[3];
-        //public int[] GunnerEquipment = new int[3];
-        //public int[] EngiEquipment = new int[3];
 
         public int MatchCount;
         public int MatchCountRecent;
@@ -159,7 +136,6 @@ namespace MatchHistoryMod
         public PlayerData(CrewSlotData playerData)
         {
             UserAvatarEntity playerEntity = playerData.PlayerEntity;
-            FileLog.Log($"Loading player data {playerData.ToString()}");
             
             Level = playerEntity.CurrentPrestigeRank;
             UserId = playerEntity.Id;
@@ -170,7 +146,6 @@ namespace MatchHistoryMod
             //playerEntity.Loadouts[playerEntity.CurrentClass].Equipments;
             Skills = playerEntity.CurrentSkills;
             MatchCountRecent = playerEntity.MatchCountInThirtyDays;
-            //playerEntity.count
 
         }
     }
--- MatchHistoryMod/MatchHistoryRecorder.cs
+++ MatchHistoryMod/MatchHistoryRecorder.cs
@@ -17,90 +17,66 @@ using System.Net;
 using System.Collections;
 using LitJson;
 
-using System.Net;
-using System.Text;
-using System.IO;
-
 namespace MatchHistoryMod
 {
     [HarmonyPatch]
     public static class MatchHistoryRecorder
     {
-
-        private const string _UploadURL = "http://127.0.0.1:80";
-
-
-        static MatchHistoryRecorder()
-        {
-        }
-
-
-        public static void SaveMatchDataLocal(LobbyData record)
-        {
-
-        }
-
         public static void UploadMatchData(LobbyData record)
         {
+            const string _UploadURL = "http://statsoficarus.xyz/submit_match_history";
             string recordJSON = JsonConvert.SerializeObject(record);
+            FileLog.Log($"Uploading match record...\n{recordJSON}");
 
+            var request = (HttpWebRequest)WebRequest.Create(_UploadURL);
+            var data = Encoding.ASCII.GetBytes(recordJSON);
+            request.Method = "POST";
+            request.Timeout = 1000;
+            request.ContentType = "application/json";
+            request.ContentLength = data.Length;
 
-
-            var httpWebRequest = (HttpWebRequest)WebRequest.Create(_UploadURL);
-            httpWebRequest.ContentType = "application/json";
-            httpWebRequest.Method = "POST";
-
-            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            try
             {
-                streamWriter.Write(recordJSON);
+                using (var stream = request.GetRequestStream())
+                {
+                    stream.Write(data, 0, data.Length);
+                }
+                FileLog.Log($"Uploaded record.");
+            }
+            catch (System.Net.WebException e)
+            {
+                FileLog.Log($"Upload failed. Server unresponsive.\n{e.ToString()}");
             }
 
-            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            try
             {
-                var result = streamReader.ReadToEnd();
+                var response = (HttpWebResponse)request.GetResponse();
+                var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                FileLog.Log($"Server responded.");
             }
-
-
+            catch (System.Net.WebException e)
+            {
+                FileLog.Log($"Upload failed. No response.\n{e.ToString()}");
+            }
+            FileLog.Log($"Upload finished.\n{recordJSON}");
         }
-        //public static void SaveMatchData(MatchData matchData)
-        //{
-        //    //FileLog.Log(JsonConvert.SerializeObject(matchData));
-        //    // Append serialized match data to match history file.
-        //    // TODO: limit size of file?
-        //    using (StreamWriter w = File.AppendText("MatchHistory.log"))
-        //    {
-        //        w.WriteLine(JsonConvert.SerializeObject(matchData));
-        //    }
-        //}
-
-        //public static void UploadMatchData(MatchData matchData)
-        //{
-            
-        //}
 
         // Called when match ends and post game screen is shown.
         [HarmonyPrefix]
         [HarmonyPatch(typeof(UIManager.UIMatchCompleteState), "Enter")]
         private static void MatchComplteStateEnter()
         {
-            FileLog.Log("NEW LOBBY DATA");
-            //var v = MatchLobbyView.Instance.GetPlayerCrew(NetworkedPlayer.Local.UserId).Match;
-            FileLog.Log("Starting Lobby data load");
-            LobbyData d = new LobbyData(MatchLobbyView.Instance, Mission.Instance);
-            FileLog.Log($"Lobby data loaded successfully");
-            FileLog.Log($"\n{JsonConvert.SerializeObject(d)}\n");
-
+            //LobbyData d = new LobbyData(MatchLobbyView.Instance, Mission.Instance);
+            //UploadMatchData(d);
 
             // Use to get time of match and some other stats
-            //FileLog.Log("Requesting match stats pre");
-            //MatchActions.GetMatchStats(new Muse.Networking.ExtensionResponseDelegate(GetMatchStats));
-            //FileLog.Log("Requesting match stats after");
+            MatchActions.GetMatchStats(new Muse.Networking.ExtensionResponseDelegate(GetMatchStats));
         }
 
 
         public static void GetMatchStats(Muse.Networking.ExtensionResponse resp)
         {
+            // TODO: remove 90% of this.
             JsonData jsonData = resp.JsonData;
             Dictionary<string, int> dictionary = new Dictionary<string, int>();
             IDictionary dictionary2 = jsonData["stats"];
@@ -190,167 +166,13 @@ namespace MatchHistoryMod
                 MuseLog.InfoFormat("no crewStats data", new object[0]);
             }
 
-            FileLog.Log($"STAT DICTIONARY");
-            foreach (KeyValuePair<string, int> entry in dictionary)
+            LobbyData d = new LobbyData(MatchLobbyView.Instance, Mission.Instance)
             {
-                FileLog.Log($"{entry.Key}: {entry.Value}");
-            }
-
-            FileLog.Log($"CREW DICTIONARY");
-            foreach (KeyValuePair<string, int> entry in dictionary5)
-            {
-                FileLog.Log($"{entry.Key}: {entry.Value}");
-            }
-            FileLog.Log($"goodbye");
-        }
-
-        public static void LogStats(IDictionary<string, int> statsData)
-        {
-            FileLog.Log("__MATCH END STATS__");
-            for (int i=0; i<statsData.Count; ++i)
-            {
-                FileLog.Log($"{statsData.ElementAt(i).Key}: {statsData.ElementAt(i).Value}\n");
-            }
+                MatchTime = dictionary5["Time Completed"]
+            };
+            UploadMatchData(d);
         }
     }
 
-    //public class MatchData
-    //{
-    //    public string MatchId;
-    //    public int GameMode;
-    //    public int MapId;
-
-    //    public int TeamCount;
-    //    public int TeamSize;
-
-    //    public int[] Scores;
-    //    public int MissionTime;
-    //    public DateTime TimeStarted;
-
-    //    public List<ShipData> Ships;
-    //    public MatchData() { }
-    //    public MatchData(MatchLobbyView matchLobbyView, Mission mission)
-    //    {
-
-    //        MatchId = matchLobbyView.MatchId;
-    //        GameMode = (int)matchLobbyView.Map.GameMode;
-    //        MapId = matchLobbyView.MapId;
-    //        TeamSize = mission.shipsPerTeam;
-    //        TeamCount = mission.numberOfTeams;
-    //        MissionTime = matchLobbyView.ElapsedTime.Seconds;
-    //        TimeStarted = DateTime.UtcNow.Subtract(TimeSpan.FromSeconds(MissionTime));
-
-    //        // Load scores from currently active mission.
-    //        int[] Scores = new int[mission.numberOfTeams];
-    //        for (int i = 0; i < mission.numberOfTeams; ++i) Scores[i] = mission.TeamScore(i);
-
-    //        // Load all the ships.
-    //        Ships = new List<ShipData>();
-    //        for (int i = 0; i < matchLobbyView.CrewCount; ++i)
-    //        {
-    //            Muse.Goi2.Entity.Vo.CrewShipVO crewShip = matchLobbyView.CrewShips[i];
-    //            CrewEntity crewEntity = matchLobbyView.FlatCrews[i];
-    //            //matchLobbyView.CrewShips
-
-    //            ShipData shipData = new ShipData(crewShip, crewEntity);
-    //            Ships.Add(shipData);
-    //        }
-    //    }
-    //}
-
-    //public class ShipData
-    //{
-    //    public int ShipId;
-    //    public string ShipName;
-
-    //    public int ShipModel;
-    //    public List<int> Guns;
-
-    //    public int TeamIdx;
-    //    public int ShipIdx;
-
-    //    public List<PlayerData> Players;
-
-    //    public ShipData(Muse.Goi2.Entity.Vo.CrewShipVO ship, CrewEntity crew)
-    //    {
-    //        ShipId = ship.Id;
-    //        ShipName = ship.Name;
-    //        ShipModel = ship.Model.Id;
-    //        TeamIdx = ship.Side;
-    //        ShipIdx = crew.SequenceInMatch;
-
-    //        Players = new List<PlayerData>();
-    //        foreach (UserAvatarEntity player in crew.CrewMembers)
-    //        {
-    //            PlayerData playerData = new PlayerData(player);
-    //            Players.Add(playerData);
-    //        }
-
-    //        // Load the guns from the ship.
-    //        // Use the guns slot names to make sure guns are added in the correct order.
-    //        string[] gunSlotNames = new string[8]
-    //        {
-    //            "gun-slot-1", "gun-slot-2", "gun-slot-3", "gun-slot-4",
-    //            "gun-slot-5", "gun-slot-6", "gun-slot-7", "gun-slot-8"
-    //        };
-    //        Guns = new List<int>();
-    //        for (int i = 0; i < ship.GetGuns().Count; ++i)
-    //        {
-    //            Guns.Add(ship.GetGuns()[gunSlotNames[i]].Id);
-    //            //Loadout.Add($"{ship.GetGuns()[gunSlotNames[i]].Id} {ship.GetGuns()[gunSlotNames[i]].NameText}");
-    //        }
-
-    //        //foreach (var item in crew.MatchStats)
-    //        //{
-    //        //    FileLog.Log($"Crew stat: {item.Key} {item.Value}");
-    //        //}
-    //    }
-    //}
-
-    //public class PlayerData
-    //{
-    //    public int PlayerId;
-    //    public string PlayerName;
-    //    public string PlayerNameRaw;
-    //    //public int? ClanId;
-    //    public string ClanTag;
-
-    //    public int CrewClass;
-    //    public int[] Levels;
-    //    //public int AvgLevel;
-    //    //public int MatchesPlayed;
-
-    //    public Dictionary<string, List<string>> Loadout;
-
-    //    public PlayerData(UserAvatarEntity user)
-    //    {
-    //        PlayerId = user.UserId;
-    //        PlayerName = user.Name;
-    //        PlayerNameRaw = user.RawName;
-    //        //ClanId = user.ClanId; // Null in this state idk why.
-    //        ClanTag = user.ClanTag;
-
-    //        Levels = new int[3] {
-    //            user.GetLevel(AvatarClass.Pilot) + user.GetPrestigeLevel(AvatarClass.Pilot) * 45,
-    //            user.GetLevel(AvatarClass.Gunner) + user.GetPrestigeLevel(AvatarClass.Gunner) * 45,
-    //            user.GetLevel(AvatarClass.Engineer) + user.GetPrestigeLevel(AvatarClass.Engineer) * 45 };
-
-    //        //AvgLevel = user.AverageLevel; // Always 0 in this state
-    //        //MatchesPlayed = NetworkedPlayer.ByUserId[user.UserId].MatchesPlayed; // Always 0 in this state
-
-    //        CrewClass = (int)user.CurrentClass;
-    //        Loadout = new Dictionary<string, List<string>>();
-    //        Loadout.Add("Pilot", new List<string>());
-    //        Loadout.Add("Gunner", new List<string>());
-    //        Loadout.Add("Engineer", new List<string>());
-    //        foreach (var skill in user.CurrentSkills)
-    //        {
-    //            var sc = CachedRepository.Instance.Get<SkillConfig>(skill);
-    //            if (sc.Type == SkillType.Helm) Loadout["Pilot"].Add($"{sc.Name} {skill}");
-    //            if (sc.Type == SkillType.Gun) Loadout["Gunner"].Add($"{sc.Name} {skill}");
-    //            if (sc.Type == SkillType.Repair) Loadout["Engineer"].Add($"{sc.Name} {skill}");
-    //        }
-    //    }
-    //}
 
 }
