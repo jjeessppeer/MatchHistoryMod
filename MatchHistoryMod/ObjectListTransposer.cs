e99e1b42fc5c8d77c6b89c3b2608bb28c6c8a0aa
Jesper Ericsson <jjeessppeer@gmail.com>
2023-11-14 17:13:33 +0100

    Moved transposer to new file

---
+++ MatchHistoryMod/ObjectListTransposer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Reflection;
+
+namespace MatchHistoryMod
+{
+    public class ObjectListTransposer<T>
+    {
+        public Dictionary<string, List<object>> Values = new Dictionary<string, List<object>>();
+
+        public ObjectListTransposer()
+        {
+            FieldInfo[] fi = typeof(T).GetFields(BindingFlags.Public | BindingFlags.Instance);
+            foreach (FieldInfo info in fi)
+            {
+                if (FieldShouldBeIgnored(info)) continue;
+                Values.Add(info.Name, new List<object>());
+            }
+        }
+
+        //public T GetInstance(int i)
+        //{
+        //}
+
+        public void Add(T newObj)
+        {
+            FieldInfo[] fi = typeof(T).GetFields(BindingFlags.Public | BindingFlags.Instance);
+            foreach (FieldInfo info in fi)
+            {
+                if (FieldShouldBeIgnored(info)) continue;
+                Values[info.Name].Add(info.GetValue(newObj));
+            }
+        }
+
+        private static bool FieldShouldBeIgnored(FieldInfo info)
+        {
+            foreach (object attr in info.GetCustomAttributes(true))
+                if (attr.GetType() == typeof(Newtonsoft.Json.JsonIgnoreAttribute))
+                    return true;
+            return false;
+        }
+    }
+
+}
--- MatchHistoryMod/ShotData.cs
+++ MatchHistoryMod/ShotData.cs
@@ -10,48 +10,9 @@ using Muse.Networking;
 using LitJson;
 using Newtonsoft.Json;
 using Muse.Goi2.Entity;
-using System.Reflection;
 
 namespace MatchHistoryMod
 {
-
-    public class ObjectListTransposer<T>
-    {
-        public Dictionary<string, List<object>> Values = new Dictionary<string, List<object>>();
-
-        public ObjectListTransposer()
-        {
-            FieldInfo[] fi = typeof(T).GetFields(BindingFlags.Public | BindingFlags.Instance);
-            foreach (FieldInfo info in fi)
-            {
-                if (FieldShouldBeIgnored(info)) continue;
-                Values.Add(info.Name, new List<object>());
-            }
-        }
-
-        //public T GetInstance(int i)
-        //{
-        //}
-
-        public void Add(T newObj)
-        {
-            FieldInfo[] fi = typeof(T).GetFields(BindingFlags.Public | BindingFlags.Instance);
-            foreach (FieldInfo info in fi)
-            {
-                if (FieldShouldBeIgnored(info)) continue;
-                Values[info.Name].Add(info.GetValue(newObj));
-            }
-        }
-
-        private static bool FieldShouldBeIgnored(FieldInfo info)
-        {
-            foreach (object attr in info.GetCustomAttributes(true))
-                if (attr.GetType() == typeof(Newtonsoft.Json.JsonIgnoreAttribute))
-                    return true;
-            return false;
-        }
-    }
-
     public class ShotData
     {
         //public int ShotIndex;
